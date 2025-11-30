6f7c93d16d0236ff2316863b0b8f74ec2b2bc380
Abel Mercado <abelitomercado0604@gmail.com>
2025-11-29 21:21:04 -0500

    Arreglando ruta BASE_URL para produccion

--- config/config.php
+++ config/config.php
@@ -1,2 +1,14 @@
 <?php
-define('BASE_URL', '/Proyecto_CDMomil/');
+// config/config.php
+
+// Detectamos si estamos en Railway revisando si existe una variable de entorno de la nube
+if (getenv('MYSQLHOST')) {
+    // ESTAMOS EN RAILWAY (Nube)
+    // Allá la ruta es la raíz del sitio
+    define('BASE_URL', '/');
+} else {
+    // ESTAMOS EN XAMPP (Local)
+    // Aquí seguimos usando tu carpeta
+    define('BASE_URL', '/Proyecto_CDMomil/');
+}
+?>
